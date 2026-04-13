a3f97c27dba57f34c0ffca2c6fdce833dc7d4c2f
may ang <mayday815@gmail.com>
2026-04-13 16:41:57 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ def main():
     # Uncomment the code below to pass the first stage
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, _ = server_socket.accept()
-    conn.sendall(b"+PONG\r\n")
     try:
         while True:
             _ = conn.recv(1024)
