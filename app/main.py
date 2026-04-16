a8246f10ef563605f24dae96713d277fe218b6c9
may ang <mayday815@gmail.com>
2026-04-15 21:24:05 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 import socket  # noqa: F401
+import threading
 
+def handle_connection(conn: socket.socket):
+    _ = conn.recv(1024)
+    conn.sendall(b"+PONG\r\n")
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -7,11 +11,10 @@ def main():
 
     # Uncomment the code below to pass the first stage
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, _ = server_socket.accept()
     try:
         while True:
-            _ = conn.recv(1024)
-            conn.sendall(b"+PONG\r\n")
+            conn, _ = server_socket.accept()
+            threading.Thread(target=handle_connection, args=(conn))
     except:
         conn.close()
 
